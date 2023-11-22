dce781e710ed90830d14f4a7d0d1a8ea1f4e5088
Zaeem Khan <hafizzaeemkhan@gmail.com>
2023-11-22 08:33:18 -0600

    Step 3: Completed

--- wc/wc.js
+++ wc/wc.js
@@ -17,12 +17,32 @@ function getNumberOfLines(fileName) {
     return splitLines.length-1;
 }
 
+function getNumberOfWords(fileName) {
+    let wordCount = 0;
+    let fileBuffer = fs.readFileSync(fileName);
+    let toString = fileBuffer.toString();
+    let splitWords = toString.replace(/[.,?!;()"'-]/g, " ")
+                            .replace(/\s+/g, " ")
+                            .replace(/\n/g, " ")
+                            .toLowerCase()
+                            .split(" ");
+    for (let word of splitWords) {
+        wordCount++;
+    }
+
+    return wordCount;
+}
+
 switch (commandList[0]) {
     case "-c":
         console.log("\t" + getBytes(fileName) + " " + fileName);
         break;
     case "-l":
         console.log("\t" + getNumberOfLines(fileName) + " " + fileName);
+        break;
+    case "-w":
+        console.log("\t" + getNumberOfWords(fileName) + " " + fileName);       
+        break;
     default:
         break;
 }
