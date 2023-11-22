55a0b50b32285d861f013221b49bf1e3ef191c99
Zaeem Khan <hafizzaeemkhan@gmail.com>
2023-11-22 08:20:15 -0600

    step 2 has been completed

--- wc/wc.js
+++ wc/wc.js
@@ -10,13 +10,19 @@ function getBytes(fileName) {
     return fileSizeInBytes;
 }
 
+function getNumberOfLines(fileName) {
+    let fileBuffer = fs.readFileSync(fileName);
+    let toString = fileBuffer.toString();
+    let splitLines = toString.split("\n");
+    return splitLines.length-1;
+}
 
 switch (commandList[0]) {
     case "-c":
-        console.log("\t" + getBytes(fileName) +" " + fileName);
+        console.log("\t" + getBytes(fileName) + " " + fileName);
         break;
-    case -l:
-        console.log("\t" + " " + fileName)
+    case "-l":
+        console.log("\t" + getNumberOfLines(fileName) + " " + fileName);
     default:
         break;
 }
